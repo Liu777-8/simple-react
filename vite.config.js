73c7e439db51e56002a8f317d488935a0b10b1a1
刘旭文 <liuxuwen@jxhsja.com>
2025-01-23 17:55:24 +0800

    feat：更新版本控制生成verson文件

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      apply: "build",
+      writeBundle() {
+        const distDir = path.resolve(__dirname, "dist");
+        if (!fs.existsSync(distDir)) {
+          fs.mkdirSync(distDir);
+        }
+
+        const versionFilePath = path.join(distDir, "version.json");
+        // 获取版本号
+        const currentVersion = uuidv4(); // 生成uuid作为版本号
+
+        fs.writeFileSync(
+          versionFilePath,
+          JSON.stringify({ version: currentVersion })
+        );
+      },
+    },
+  ],
+});
